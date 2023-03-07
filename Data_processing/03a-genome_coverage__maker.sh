ec48afdf957d9e97586392478fabc34623893e5c
Clockris <kris.petrini@gmail.com>
2023-03-07 19:08:07 +0100

    Bug fix and working example command added

--- Data_processing/03a-genome_coverage__maker.sh
+++ Data_processing/03a-genome_coverage__maker.sh
@@ -1,3 +1,5 @@
+echo "Working example: sh 03a-genome_coverage__maker.sh Alignment_test/03-test_filtered.bam test.bw bigwig 4 1000 $hg38_unalignable 2701495761 RPKM 250"
+
 # INPUT
 file=$1 # Input file (.bam)
 output_file=$2 # Path to the output file
@@ -9,6 +11,12 @@ genomesize=$7 # Estimated genome size to performing the normalization
 normalization_method=$8 # Method selecte to perform the normalizazion (e.g. "RPKM") 
 extend=$9 # Number expressing (in base pairs) how much a reads should be extended (e.g. 250) 
 
+bamCoverage="/path/to/bamCoverage"
+
+### Personal paths ###
+bamCoverage="/storage/home/cpetrini/Programs/DeepTools/bin/bamCoverage"
+######################
+
 # PROCESSING
 $bamCoverage -b $file \
 	    -o $output_file \
@@ -19,3 +27,5 @@ $bamCoverage -b $file \
             --effectiveGenomeSize $genomesize \
             --normalizeUsing $normalization_method \
 	    --extendReads $extend
+
+# Working example: sh 03a-genome_coverage__maker.sh Alignment_test/03-test_filtered.bam test.bw bigwig 4 1000 $hg38_unalignable 2701495761 RPKM 250
